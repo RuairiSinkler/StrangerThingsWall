6dbff83177925916668ede0829ce1008ac6e2919
Fuzznut <sinky_son@hotmail.co.uk>
2017-10-17 23:53:58 +0100

    Changed exception name

--- Wall.py
+++ Wall.py
@@ -157,7 +157,7 @@ class Wall:
                     print()
                 print("Twitter Check Sleeping")
                 time.sleep(20)
-            except twitter.TwitterError:
+            except twitter.socket.gaieerror:
                 print("Error getting tweets, retrying in 60 seconds")
                 time.sleep(60)
 
